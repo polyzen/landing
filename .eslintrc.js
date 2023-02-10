4219d526552717fbe0e4ccd571f7df8740a34eaa
Daniel M. Capella <polyzen@archlinux.org>
2023-02-10 00:07:42 -0500

    Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
   root: true,
-  plugins: ["@html-eslint"],
+  plugins: ['@html-eslint'],
   overrides: [
     {
-      files: ["*.html"],
-      parser: "@html-eslint/parser",
-      extends: ["plugin:@html-eslint/recommended"],
+      files: ['*.html'],
+      parser: '@html-eslint/parser',
+      extends: ['plugin:@html-eslint/recommended'],
       rules: {
-        "@html-eslint/indent": "off",
-        "@html-eslint/require-closing-tags": "off",
+        '@html-eslint/indent': 'off',
+        '@html-eslint/no-extra-spacing-attrs': [
+          'error',
+          { enforceBeforeSelfClose: true },
+        ],
+        '@html-eslint/require-closing-tags': [
+          'error',
+          { selfClosing: 'always' },
+        ],
       },
     },
   ],
