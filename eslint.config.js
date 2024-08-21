94390d3421502b5d8a658c7d99d041c87c3c6fb8
Daniel M. Capella <polyzen@archlinux.org>
2024-08-20 22:53:55 -0400

    Use ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import html from '@html-eslint/eslint-plugin';
+
+/** @type { import('eslint').Linter.FlatConfig[] } */
+const config = [
+  {
+    ...html.configs['flat/recommended'],
+    files: ['**/*.html'],
+    rules: {
+      ...html.configs['flat/recommended'].rules,
+      '@html-eslint/indent': 'off',
+      '@html-eslint/no-extra-spacing-attrs': [
+        'error',
+        { enforceBeforeSelfClose: true },
+      ],
+      '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }],
+    },
+  },
+];
+
+export default config;
